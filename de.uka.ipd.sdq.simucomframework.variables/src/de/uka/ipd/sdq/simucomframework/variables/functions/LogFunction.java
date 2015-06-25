d7383717e5b5180b65d5d37780e6a7fca156a3c5
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-06-25 10:42:56 +0000

    Code cleanup
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Analyzer/SimuCom/trunk@27859 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.simucomframework.variables/src/de/uka/ipd/sdq/simucomframework/variables/functions/LogFunction.java
+++ de.uka.ipd.sdq.simucomframework.variables/src/de/uka/ipd/sdq/simucomframework/variables/functions/LogFunction.java
@@ -6,56 +6,65 @@ public class LogFunction implements IFunction {
     /** Name used in the stochastic expression for this function. */
     public static final String LOG_FUNCTION_NAME = "Log";
 
-    public boolean checkParameters(List<Object> parameters) {
-        if (parameters.size() != 2)
+    @Override
+    public boolean checkParameters(final List<Object> parameters) {
+        if (parameters.size() != 2) {
             return false;
-        
+        }
+
         // base
         if (parameters.get(0) instanceof Double) {
-        	Double base = (Double) parameters.get(0);
-        	if(base<=0 || base==1)
-        		return false;
+            final Double base = (Double) parameters.get(0);
+            if(base<=0 || base==1) {
+                return false;
+            }
         }
         else if (parameters.get(0) instanceof Integer) {
-        	Integer base = (Integer) parameters.get(0);
-        	if(base<=0 || base==1)
-        		return false;
+            final Integer base = (Integer) parameters.get(0);
+            if(base<=0 || base==1) {
+                return false;
+            }
         }
         else {
             return false;
         }
-        
+
         // value
         if (parameters.get(1) instanceof Double) {
-        	Double value = (Double) parameters.get(1);
-        	if(value <= 0)
-        		return false;
+            final Double value = (Double) parameters.get(1);
+            if(value <= 0) {
+                return false;
+            }
         }
         else if (parameters.get(1) instanceof Integer) {
-        	Integer value = (Integer) parameters.get(1);
-        	if(value <= 0)
-        		return false;
+            final Integer value = (Integer) parameters.get(1);
+            if(value <= 0) {
+                return false;
+            }
         }
         else {
             return false;
         }
-        
+
         return true;
     }
 
-    public Object evaluate(List<Object> parameters) {
-    	Double base;
-        if (parameters.get(0) instanceof Integer)
-    		base = (double)(int)parameters.get(0);
-        else
-        	base = (Double) parameters.get(0);
-        
-    	Double value;
-        if (parameters.get(1) instanceof Integer)
-        	value = (double)(int)parameters.get(1);
-        else
-        	value = (Double) parameters.get(1);
-    	
+    @Override
+    public Object evaluate(final List<Object> parameters) {
+        Double base;
+        if (parameters.get(0) instanceof Integer) {
+            base = (double)(Integer)parameters.get(0);
+        } else {
+            base = (Double) parameters.get(0);
+        }
+
+        Double value;
+        if (parameters.get(1) instanceof Integer) {
+            value = (double)(Integer)parameters.get(1);
+        } else {
+            value = (Double) parameters.get(1);
+        }
+
         return Math.log(value)/Math.log(base);
     }
 
--- de.uka.ipd.sdq.simucomframework.variables/src/de/uka/ipd/sdq/simucomframework/variables/functions/SqrtFunction.java
+++ de.uka.ipd.sdq.simucomframework.variables/src/de/uka/ipd/sdq/simucomframework/variables/functions/SqrtFunction.java
@@ -6,21 +6,26 @@ public class SqrtFunction implements IFunction {
     /** Name used in the stochastic expression for this function. */
     public static final String SQRT_FUNCTION_NAME = "Sqrt";
 
-    public boolean checkParameters(List<Object> parameters) {
-        if (parameters.size() != 1)
+    @Override
+    public boolean checkParameters(final List<Object> parameters) {
+        if (parameters.size() != 1) {
             return false;
-        if (!(parameters.get(0) instanceof Double) && !(parameters.get(0) instanceof Integer))
+        }
+        if (!(parameters.get(0) instanceof Double) && !(parameters.get(0) instanceof Integer)) {
             return false;
+        }
         return true;
     }
 
-    public Object evaluate(List<Object> parameters) {
-    	double value;
-        if (parameters.get(0) instanceof Integer)
-            value = (int)parameters.get(0);
-        else
-        	value = (double)parameters.get(0);
-        
+    @Override
+    public Object evaluate(final List<Object> parameters) {
+        double value;
+        if (parameters.get(0) instanceof Integer) {
+            value = (Integer)parameters.get(0);
+        } else {
+            value = (Double)parameters.get(0);
+        }
+
         return Math.sqrt(value);
     }
 
