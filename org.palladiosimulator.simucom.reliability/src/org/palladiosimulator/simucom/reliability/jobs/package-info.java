c11dc7bc50962bdbd385131c2d13f180ba9edcc0
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-25 00:32:35 +0000

    integral part of simucom
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Analyzer/SimuCom/trunk@27786 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.simucom.reliability/src/org/palladiosimulator/simucom/reliability/jobs/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes to define reliability-specific
+ * configuration options for SimuCom runs, as well as the UI
+ * support to control those options.
+ */
+package org.palladiosimulator.simucom.reliability.jobs;
+
