8d89a463a7370dfc3d70111ec010500eb705d1ee
Sebastian Krach <krach@fzi.de>
2018-06-18 18:57:20 +0200

    Split of SimuCom repository. Removed unrelated projects.

--- de.uka.ipd.sdq.pcm.codegen.simucom/src-transforms/de/uka/ipd/sdq/pcm/codegen/simucom/transformations/AllocationXpt.xtend
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.pcm.codegen.simucom.transformations
-
-import org.palladiosimulator.pcm.allocation.Allocation
-
-abstract class AllocationXpt {
-	def String allocationTM(Allocation allocation)
-}
--- org.palladiosimulator.simucom.reliability/src/org/palladiosimulator/simucom/reliability/jobs/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains classes to define reliability-specific
- * configuration options for SimuCom runs, as well as the UI
- * support to control those options.
- */
-package org.palladiosimulator.simucom.reliability.jobs;
-
